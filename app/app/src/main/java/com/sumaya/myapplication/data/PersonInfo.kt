66af4d4fb924f7a902387f9fbf9c0ea46da45d37
Hani <hani.dabsh.jed.k1@tuwaiq.edu.sa>
2021-11-02 21:03:27 +0300

    done

---
+++ app/app/src/main/java/com/sumaya/myapplication/data/PersonInfo.kt
@@ -0,0 +1,12 @@
+package com.sumaya.myapplication.data
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class PersonInfo(
+    val name:String,
+    val birthday:String,
+    val phone: String,
+    val gender:String
+): Parcelable
